1245f20701027cf27c82517d564f4f16a77896c3
quirinof <matheusquinoff@gmail.com>
2024-01-25 09:20:01 -0300

    add menu click

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('s')
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
