dc6dc69ef44e7a54649430980d659cef5e6112a3
quirinof <matheusquinoff@gmail.com>
2024-02-28 21:33:34 -0300

    add breakpoints on swiper - adjust testimonials section

--- main.js
+++ main.js
@@ -32,14 +32,20 @@ function changeHeaderWhenScroll() {
 }
 
 
-/* testiomonials carousel slider swiper */
+/* testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination',
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 
