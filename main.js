356dde643c9ff79a5530286f4a9aefe6702a44f4
quirinof <matheusquinoff@gmail.com>
2024-02-29 23:41:27 -0300

    activate menu

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for(const link of links) {
 }
 
 /* change header on page when scroll */
+const header = document.querySelector("header")
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector("header")
-  const navHeight = header.offsetHeight
-  
+
   if(window.scrollY >= navHeight) {
     // scrol maior que altura do header
     header.classList.add('scroll')
@@ -70,8 +70,8 @@ scrollReveal.reveal(
 
 
 /* Button back-to-top */ 
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
 
   if(window.scrollY >= 560) {
     backToTopButton.classList.add('show')
@@ -80,8 +80,32 @@ function backToTop() {
   }
 }
 
+/* active menu items */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    } else {
+      document.querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
+}
+
 /* when scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
